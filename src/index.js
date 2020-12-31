d60fbef61e91140f4b8dff6a784d383689cea3e6
Brandon Pessman <34040658+BrandonPessman@users.noreply.github.com>
2020-12-30 22:11:49 -0700

    Fix ga

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-133137008-3');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
